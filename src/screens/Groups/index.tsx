9de4448e1b35fd94364c0be841ea80c142fb92ec
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-10-16 05:09:40 -0300

    feat/config: Group Screen created + Path Mapping in imports

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export function Groups() {
    return (
-      <View>
-         <Text>Pagina Groups</Text>
+      <View style={styles.container}>
+         <Text style={styles.text}>Pagina Groups</Text>
       </View>
    );
 }
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#000000',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+   text: {
+      color: '#FFFFFF',
+      fontSize: 15,
+   }
+})
