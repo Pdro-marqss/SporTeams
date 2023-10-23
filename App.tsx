408e600a583d3d3f4d64cec689b690abc41eb580
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-10-23 19:57:16 -0300

    feat: add fonts + load component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
 
+import { Loading } from '@components/Loading';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Groups /> : <Loading />}
       <StatusBar style="light" />
     </ThemeProvider>
 
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+   background-color: ${props => props.theme.COLORS.GRAY_600};
+
+   flex: 1;
+   justify-content: center;
+   align-items: center;
+`;
+
+export const LoadIndicator = styled.ActivityIndicator.attrs((props) => ({
+   color: props.theme.COLORS.GREEN_700,
+}))``;
