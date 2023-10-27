b84c214b512e5d0f17c908352233a94b75b144dd
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-10-27 15:59:05 -0300

    feat: highlightComponent added

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/highlight';
+
 import { Container } from './styles'; //Da pra usar Alias aqui da seguinte forma: import * as S from './styles'. E puxar os components do S -> <S.Container></S.Container>
 
 export function Groups() {
    return (
       <Container>
          <Header />
+         <Highlight
+            title='Turmas'
+            subtitle='Jogue com a sua turma'
+         />
       </Container>
    );
 }
