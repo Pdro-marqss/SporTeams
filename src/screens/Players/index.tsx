25568178f91b5891bf7773010b27ed81195150ed
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-11-13 23:56:23 -0300

    feat: filterComponent + number of players in team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+   const [team, setTeam] = useState('Time A');
+   const [players, setPlayers] = useState([]);
+
    return (
       <Container>
          <Header showBackButton />
@@ -23,10 +29,25 @@ export function Players() {
             <ButtonIcon icon="add" />
          </Form>
 
-         <Filter
-            title="Time A"
+         <HeaderList>
+            <FlatList
+               data={['Time A', 'Time B']}
+               keyExtractor={item => item}
+               renderItem={({ item }) => (
+                  <Filter
+                     title={item}
+                     isActive={item === team}
+                     onPress={() => setTeam(item)}
+                  />
+               )}
+               horizontal={true}
+            />
+            <NumberOfPlayers>
+               {players.length}
+            </NumberOfPlayers>
+         </HeaderList>
 
-         />
+
       </Container>
    );
 }
